2d01ebbcfcac898e9e9e0f29657ecf02f13b3a26
Peter Wolf <pewobox@Peters-Air.fritz.box>
2020-11-10 14:11:42 +0100

    react-bootstrap integration | connection of react bootstrap to react project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css"
 import Profile from './Profile';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
 function App() {
+
   return (
    <Router>
       <div className="App">
-        <Profile />
+          <Profile />
       </div>
    </Router> 
     
--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {Button, Card} from "react-bootstrap";
 
-function Profile() {
+
+export default function Profile(props) {
   return (
     <div>
-      <h1>Profile</h1>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+
+
     </div>
   );
 }
-
-export default Profile;
