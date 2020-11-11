ded5133fc5579899ea1b03f99b2cd8952f857de5
Peter Wolf <pewobox@Peters-Air.fritz.box>
2020-11-11 15:52:32 +0100

    added images and failed to manage the skills as seperate icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../App.css';
-import { Card} from "react-bootstrap";
+import { Card, Badge} from "react-bootstrap";
+import Skills from "./Skills";
+
 
 
 export default function Profile(props) {
-  return (
-      <div>
-        <Card style={{ width: '18rem', margin: '2px'}}>
-            <Card.Img variant="top" src="" />
-            <Card.Body>
-                <Card.Title>{props.profileValue.firstName} {props.profileValue.lastName}</Card.Title>
-                <Card.Text>
-                    {props.profileValue.profileDescription}
-                </Card.Text>
-                <a href={`mailto:${props.profileValue.email}?subject=Request a new employee`} target="_blank" className="btn btn-primary m-2">Send Email</a>
-                <br />
-                <a href="#" target="_blank" className="btn btn-primary m-2">LinkedIn</a>
-                <a href="#" target="_blank" className="btn btn-primary m-2">GitHub</a>
-            </Card.Body>
-        </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card style={{width: '18rem', margin: '2px'}}>
+                <Card.Img style={{height: '150px'}} variant="top"
+                          src={props.profileValue.profileImage.fields.file.url}/>
+                <Card.Body>
+                    <Card.Title>{props.profileValue.firstName} {props.profileValue.lastName}</Card.Title>
+                    <Card.Text>
+                        {props.profileValue.profileDescription}
+                    </Card.Text>
+                    <Badge className="m-2" variant="secondary">skillz</Badge>
+                    <br/>
+                    <a href={`mailto:${props.profileValue.email}?subject=Request a new employee`} target="_blank"
+                       className="btn btn-primary mb-4">Send Email</a>
+                    <br/>
+                    <a href="#" target="_blank" className="btn btn-secondary m-2">LinkedIn</a>
+                    <a href="#" target="_blank" className="btn btn-secondary m-2">GitHub</a>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+
+export default function Skills(props){
+    console.log(props.userSkill)
+
+    return(
+        <Card.Link>
+           jj
+        </Card.Link>
+    )
+
+
+}
